e4d9f4e8b3b2127e837972ba21aa561f0ae07753
NathalieLima <nathalielima3@gmail.com>
2023-11-22 13:48:22 -0300

    Aula do dia 22/11

---
+++ Introducao/semana3/exemplos-sala/sld06/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Crie um programa que solicita ao usuário que insira um número. Utilize um bloco try-catch para
+lidar com a possível exceção gerada se o usuário inserir algo que não seja um número. No bloco
+catch, exiba uma mensagem amigável informando ao usuário que um número válido deve ser
+inserido.
+*/
+
+int number;
+
+try {
+    Console.Write("Insira um número: ");
+    number = int.Parse(Console.ReadLine());
+} catch (Exception er) {
+    Console.WriteLine($"Insira um número válido!");
+    
+}
+finally {
+    Console.WriteLine($"{number}");
+}
+
+
