09cba25cad41f35c1a57608846fc2fb70d0047c3
NathalieLima <nathalielima3@gmail.com>
2023-12-14 17:37:21 -0300

    Segunda parte da aula do dia 14/12

---
+++ TrilhaNET-CLI/Domain/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain;
+public static class User
+{
+    public static string Name => "Náthalie Lima";
+    public static List<(string, int)> Skills => new List<(string, int)>{
+        ("Fundamentos de C#", 5)
+    };
+}
